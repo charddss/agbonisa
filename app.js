7543dde6024953cbcb8c9d3742b7e9c57978fd4b
charddss <richardagbonisa202300406@gmail.com>
2024-09-23 16:30:18 +0800

    updated

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 function myFunction() {
-    alert("The form was submitted");
-
-   let name = document.getElementById("name").value
-   let age = document.getElementById("age").value
-   let email = document.getElementById("email").value
-   let hobby = document.getElementById("hobby").value
-   console.log(name)
-   console.log(age)
-   console.log(email)
-   console.log(hobby)
-
-   const user ={
-    name: name,
-    age: age,
-    email: email,
-    hobby: hobby
-   }
    
-   const myJSON = JSON.stringify(user);
-   console.log(user)
-   console.log(myJSON)
-   document.getElementById("jsonOutput").innerHTML  = myJSON
-  }
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    const email = document.getElementById("email").value;
+    const hobby = document.getElementById("hobby").value;
+
+    
+    const jsonData = {
+        name: name,
+        age: age,
+        email: email,
+        hobby: hobby
+    };
+
+    document.getElementById("jsonOutput").textContent = JSON.stringify(jsonData, null, 2);
+}
